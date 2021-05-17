34659fda87043d13beddef06c8138b70ddfdd5cb
michzelinger <mzelin@uw.edu>
2021-05-17 11:51:03 -0700

    nothing really

---
+++ LostInSpace/Assets/Scripts/Scenes/SceneTransition.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    // public Animator transitionAnim;
+
+    // void Update()
+    // {
+    //     if(Input.)
+    // }
+}
