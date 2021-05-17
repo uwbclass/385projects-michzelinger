78c91083a48eabbee1e534f669ff9066966eb6e7
awesomeedl <tliao512@uw.edu>
2021-05-17 11:48:32 -0700

    invulnerable time, trail during speed boost, optimizations

---
+++ LostInSpace/Assets/Scripts/Boundaries/boundary.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundary : MonoBehaviour
+{
+    public Vector2 boundarySize;
+    public GameObject top;
+    public GameObject bottom;
+    public GameObject left;
+    public GameObject right;
+
+    void Awake()
+    {
+        top.transform.localPosition = new Vector2(0,(boundarySize.y + 0.5f));
+        bottom.transform.localPosition = new Vector2(0, -(boundarySize.y + 0.5f));
+        left.transform.localPosition = new Vector2(-(boundarySize.x + 0.5f), 0);
+        right.transform.localPosition = new Vector2((boundarySize.x + 0.5f), 0);
+
+        top.GetComponent<BoxCollider2D>().size = new Vector2(boundarySize.x * 2 + 2, 1);
+        bottom.GetComponent<BoxCollider2D>().size = new Vector2(boundarySize.x * 2 + 2, 1);
+        left.GetComponent<BoxCollider2D>().size = new Vector2(1, boundarySize.y * 2 + 2);
+        right.GetComponent<BoxCollider2D>().size = new Vector2(1, boundarySize.y * 2 + 2);
+    }
+    void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Vector2 border = boundarySize;
+        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
+    }
+}
